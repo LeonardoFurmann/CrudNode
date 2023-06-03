9b311ea249e549fb37168bd5828f8a54015a87f4
Leonardo Furmann <leohitnerf@gmail.com>
2023-06-03 18:09:42 -0300

    outras consultas

--- trabalho/controllers/assinanteController.js
+++ trabalho/controllers/assinanteController.js
@@ -17,12 +17,94 @@ class AssinanteController {
     async buscarPorId(req, res) {
         const id = req.params.id;
         const resultado = await assinanteModel.findOne({ 'id': id });
+
+        if(!resultado){
+            res.status(404).json({msg: "Assinante não encontrado"});
+            return;
+        }
+
         res.status(200).json(resultado);
     }
+
+    async buscarPorNome(req, res) {
+        const nome = req.params.nome;
+        const resultado = await assinanteModel.find({nome});
+        
+        if(!resultado){
+            res.status(404).json({msg: "Assinante não encontrado"});
+            return;
+        }
+
+        res.status(200).json(resultado);
+    }
+
+    async buscarPorSobrenome(req, res) {
+        const sobrenome = req.params.sobrenome;
+        const resultado = await assinanteModel.find({sobrenome});
+        
+        if(!resultado){
+            res.status(404).json({msg: "Assinante não encontrado"});
+            return;
+        }
+        
+        res.status(200).json(resultado);
+    }
+
+    async buscarPorCidade(req, res) {
+        const cidade = req.params.cidade;
+        const resultado = await assinanteModel.find({cidade});
+        
+        if(!resultado){
+            res.status(404).json({msg: "Assinante não encontrado"});
+            return;
+        }
+        
+        res.status(200).json(resultado);
+    }
+
+    async buscarPorStatus(req, res) {
+        const status = req.params.status;
+        let resultado;
+
+        if (status == "Ativo") {
+            resultado = await assinanteModel.find({status: 'true'});
+        }
+
+        if (status == "Inativo") {
+            resultado = await assinanteModel.find({status: 'false'});
+        }
+    
+        if(!resultado){
+            res.status(404).json({msg: "Assinante não encontrado"});
+            return;
+        }
+        
+        res.status(200).json(resultado);
+    }
+
+    async buscarPorEstado(req, res) {
+        const estado = req.params.estado;
+        const resultado = await assinanteModel.find({estado});
+        
+        if(!resultado){
+            res.status(404).json({msg: "Assinante não encontrado"});
+            return;
+        }
+        
+        res.status(200).json(resultado);
+    }
+
+
 
     async atualizar(req, res) {
         const id = req.params.id;
         const _id = String((await assinanteModel.findOne({ 'id': id }))._id);
+
+        if(!_id){
+            res.status(404).json({msg: "Assinante não encontrado"});
+            return;
+        }
+
         await assinanteModel.findByIdAndUpdate(String(_id), req.body);
         res.status(200).send();
     }
--- trabalho/routes/assinantesRouter.js
+++ trabalho/routes/assinantesRouter.js
@@ -5,6 +5,11 @@ const assinanteController = require('../controllers/assinanteController');
 router.get('/assinantes', assinanteController.listar);
 router.post('/', assinanteController.salvar);
 router.get('/:id', assinanteController.buscarPorId);
+router.get('/nome/:nome', assinanteController.buscarPorNome);
+router.get('/estado/:estado', assinanteController.buscarPorEstado);
+router.get('/sobrenome/:sobrenome', assinanteController.buscarPorSobrenome);
+router.get('/cidade/:cidade', assinanteController.buscarPorCidade);
+router.get('/status/:status', assinanteController.buscarPorStatus);
 router.put('/:id', assinanteController.atualizar);
 router.delete('/:id', assinanteController.excluir);
 
